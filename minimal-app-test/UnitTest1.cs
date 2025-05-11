74334a804169a983f8abbfb4e6c6ee9f5d8ef7d2
Shulamit Petranker <shulamitp@altshare.com>
2025-05-11 10:10:55 +0300

    commit2

---
+++ minimal-app-test/UnitTest1.cs
@@ -0,0 +1,45 @@
+namespace minimal_app_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetMessage_WithValidConnectionString_ReturnsMessageWithConnection()
+        {
+            // Arrange
+            string dbConnection = "Server=myServer;Database=myDb;";
+
+            // Act
+            var result = Logic.GetMessage(dbConnection);
+
+            // Assert
+            Assert.Equal("hello word - Server=myServer;Database=myDb;", result);
+        }
+
+        [Fact]
+        public void GetMessage_WithNullConnectionString_ReturnsMessageWithNull()
+        {
+            // Arrange
+            string? dbConnection = null;
+
+            // Act
+            var result = Logic.GetMessage(dbConnection);
+
+            // Assert
+            Assert.Equal("hello word - ", result);
+        }
+
+        [Fact]
+        public void GetMessage_WithEmptyConnectionString_ReturnsMessageWithEmpty()
+        {
+            //test my tests ----------
+            // Arrange
+            string dbConnection = "";
+
+            // Act
+            var result = Logic.GetMessage(dbConnection);
+
+            // Assert
+            Assert.Equal("hello word - ", result);
+        }
+    }
+}
---
+++ minimal-app/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var dbConnection = Environment.GetEnvironmentVariable("DB_CONNECTION");
+app.MapGet("/", () => Results.Text(Logic.GetMessage(dbConnection)));
+
+
+app.Run();
+
+
+public static class Logic
+{
+    public static string GetMessage(string? dbConnection)
+    {
+        //if (string.IsNullOrEmpty(dbConnection))
+        //    return "almost there, secret: DB_CONNECTION is not set";
+
+        return $"hello word - {dbConnection}";
+    }
+}
